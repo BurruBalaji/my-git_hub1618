af461076204c5d066e2e2899cb2c9a9e1fe72fc0
Balaji <burrubalaji@gmail.com>
2023-09-14 23:15:32 +0530

    added second local heading

--- bb_client/src/App.js
+++ bb_client/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This is my first git hub project</h1>
+      <h1>This is second heading from local repository</h1>
     </div>
   );
 }
